41eecce29e59292ea0a0cf67a1fafcd5245f9c08
zacks08 <isaacchags011@gmail.com>
2024-08-21 14:41:09 -0300

    responsibilidade
    mudança para deixalo mais responsivo e minimalista para outros aparelhos

---
+++ index.js
@@ -0,0 +1,5 @@
+// Este script pode ser usado para adicionar funcionalidades ao carrossel no futuro, 
+// como navegação automática, botões de próximo/anterior, etc. Por enquanto, deixei vazio, 
+// pois o carrossel já é funcional com CSS e HTML.
+
+console.log("Script carregado com sucesso.");
